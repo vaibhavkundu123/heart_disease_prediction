c63484ed1295aebc20c68ca9e776b3835b54a7a1
Vaibhav Kundu <vaibhavkundu69@gmail.com>
2024-10-25 16:32:35 +0530

    email_process_corrected

--- heart_disease_prediction/settings.py
+++ heart_disease_prediction/settings.py
@@ -132,6 +132,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'vaibhavkundu69@gmail.com'
 EMAIL_HOST_PASSWORD = 'yslw xxox negx mjxg'
 DEFAULT_FROM_EMAIL = 'noreply@mywebsite.com'
